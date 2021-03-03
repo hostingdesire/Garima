5efc92b3959ba452808bb4b7e5fca75ffbd9d4e4
garima1820 <67752635+garima1820@users.noreply.github.com>
2021-03-03 12:06:01 +0530

    takes pictures using webcam and store it to s3 bucket

--- webcam_pics.py
+++ webcam_pics.py
@@ -5,14 +5,14 @@ from PIL import Image
 
 s3_client = boto3.client('s3')
 # a=int(input('enter number of imgs u want to be clicked: '))
-a=10	//assume it'll click only 10 pics
+a=10	//u can change no. of clicks acc to ur requirement
 camera = cv2.VideoCapture(0 , cv2.CAP_DSHOW)
 for i in range(a):
     return_value, image = camera.read()
 
     image_string = cv2.imencode('.jpg', image)[1].tobytes()
 
-    s3_client.put_object(Bucket="demo-12345", Key = 'imgs/img'+str(i)+'.png', Body=image_string, ContentType='png')   //create S3 bucket and replace name with ur S3 bucket name
+    s3_client.put_object(Bucket="abc12345", Key = 'img'+str(i)+'.png', Body=image_string, ContentType='png')   //create S3 bucket and replace name with ur S3 bucket name
     print(i)
     time.sleep(5)
 del(camera)
